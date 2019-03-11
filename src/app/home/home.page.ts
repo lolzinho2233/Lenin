062bbe9649f055ede07a656f2515e00054bd79e7
lolzinho2233 <leninlevandovisk2@gm ail.com>
2019-03-11 12:09:21 -0300

    segunda versão

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  nome : string = "BOM DIA!"
+  clicar() : void{
+    alert("GOOD DAY TO YOU")
+  }
 }
